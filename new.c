7ca6fcdcf7b69f5a963aa56e1b8163e4d000f4ab
Mel-SRK <3203216748@qq.com>
2025-01-09 13:22:41 +0800

    Update

--- new.c
+++ new.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-//#include <stdlib.h> //这两个是头文件
+//#include <stdlib.h> 
 
-int main() //函数的声明方式好像，毕竟初学，也不懂
+int main()
 {
-	printf("hello,world\n"); //printf函数，在终端打印，\是编一个转译字符
+	printf("hello,world\n");
     int a = 1;
     printf("%d",a);
-	return 0;//返回值为0，表示没有错误
+	return 0;
 }
 
