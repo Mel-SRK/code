0783b4ca1d3191a14200943d538b2e9709c972c2
Mel-SRK <3203216748@qq.com>
2026-01-16 18:09:27 +0800

    Updata somethin...

--- C/luogu/new.c
+++ C/luogu/new.c
@@ -1,33 +1,24 @@
 #include<stdio.h>
-int a[20][20];
 
 int main(){
-    int n,k=0,x=0,y=0;
+    int n;
     scanf("%d",&n);
-    while(k<=n*n){
-        while(x<n-1&&a[x+1][y]==0){
-            a[x][y]=++k;
-            ++x;
+    int num;
+    while (scanf("%d",&num))
+    {
+        for (size_t i = 0; i < num/n; i++)
+        {
+            for (size_t j = 0; j < num&&j<n; i++)
+            {
+                /* code */
+            }
+            if (condition)
+            {
+                /* code */
+            }
+            
         }
-        while(y<n-1&&a[x][y+1]==0){
-            a[x][y]=++k;
-            ++y;
-        }
-        while(x>0&&a[x-1][y]==0){
-            a[x][y]=++k;
-            --x;
-        }
-        while(y>0&&a[x][y-1]==0){
-            a[x][y]=++k;
-            --y;
-        }
+        
     }
-    for(int i=0;i<n;++i){
-        for(int j=0;j<n;++j){
-            if(a[j][i]<10)printf(" ");
-            printf(" %d",a[j][i]);
-        }
-        printf("\n");
-    }
-    return 0;
+    
 }
